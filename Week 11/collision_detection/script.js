fd63314fe96debe21ae72dd1026b257e9db8936f
JulieONeill <79820551+JulieONeill@users.noreply.github.com>
2021-05-17 17:58:31 +1200

    Update script.js

--- Week 11/collision_detection/script.js
+++ Week 11/collision_detection/script.js
@@ -9,3 +9,78 @@ Next, add the following features:
 - the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
 
 */
+
+var puck = document.getElementById('puck');
+var gap = document.getElementById('gap');
+var puck_x = 210;
+var puck_y = 210;
+var speed = 50;
+
+function logcoordinates() {
+  console.log('x=' + puck_x + 'y=' + puck_y);  
+}
+
+function checkCollision() {
+   if(puck_x >= 250 && puck_x <= 350){
+       if (puck_y >= 150 && puck_y <=250){
+          console.log('collision!'); 
+       {document.getElementById('gap').style.backgroundColor = 'red';
+   }}}
+    else { 
+        document.getElementById('gap').style.backgroundColor = 'white';
+         }
+    
+     if(puck_y >= 150 && puck_y <= 250){
+       if (puck_x >= 250 && puck_x <=350){
+          console.log('collision!'); 
+       {document.getElementById('gap').style.backgroundColor = 'red';
+   }}}
+    else { 
+        document.getElementById('gap').style.backgroundColor = 'white';
+         }
+    
+    console.log('check');
+}
+
+function checkBorder (){
+    if (puck_x <=0){
+        console.log('border')
+        puck_x == 600 + 'px'
+            }}
+ 
+puck.style.top = puck_y + 'px';
+puck.style.left = puck_x + 'px';
+
+document.getElementById('left').addEventListener('click', () => {
+    puck_x -= speed; 
+    puck.style.left = puck_x + 'px';
+   logcoordinates();
+    checkCollision();
+    checkBorder ();
+});
+
+document.getElementById('right').addEventListener('click', () => {
+    puck_x += speed; 
+    puck.style.left = puck_x + 'px';
+     logcoordinates();
+     checkCollision();
+    checkBorder ();
+});
+
+document.getElementById('up').addEventListener('click', () => {
+    puck_y -= speed; 
+    puck.style.top = puck_y + 'px';
+     logcoordinates();
+     checkCollision();
+    checkBorder ();
+});
+
+
+document.getElementById('down').addEventListener('click', () => {
+    puck_y += speed; 
+    puck.style.top = puck_y + 'px';
+     logcoordinates();
+     checkCollision();
+    checkBorder ();
+});
+
